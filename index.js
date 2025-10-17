f6b286be4f9eb54f0a80eadf791a0a4e75182d3d
Oluwaseun Adesina <Oluwaseunadesina8@gmail.com>
2025-10-17 21:39:03 +0100

    first deployment

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/me', async (req, res) => {
+    try {
+        const response = await axios.get('https://catfact.ninja/fact', {
+            // timeout: 5000
+        })
+
+        const fact = response.data.fact;
+
+        const data = {
+            status: "success",
+            user: {
+                email: "oluwaseunadesina8@gmail",
+                name: "Oluwaseun Adesina",
+                stack: ["JavaScript", "Nodejs", "Expressjs"],
+            },
+            timestamp: new Date().toISOString(),
+            fact: fact,
+        }
+
+        res.setHeader('Content-Type', 'application/json');
+        return res.status(200).json(data);
+    } catch (err) {
+        console.error('Error fetching cat fact:', err.message);
+        return res.status(500).json({ status: "error", message: "Failed to fetch cat fact" });
+    }
+});
+
+app.listen(PORT, () =>{
+    console.log(`Server is running on port ${PORT}`);
+})
